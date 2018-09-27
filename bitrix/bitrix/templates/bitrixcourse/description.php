4ab6b717a5047d4ed5ab17d5d838f08860d15676
leonidvilis <leonidvilis@gmail.com>
2018-09-27 08:41:47 +0300

    Added project for integrates

---
+++ bitrix/bitrix/templates/bitrixcourse/description.php
@@ -0,0 +1,9 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+$arTemplate = Array(
+	'NAME' => 'BitrixCourse',
+	'DESCRIPTION' => 'Шаблон сайта интегрированный по курсу битрикс'
+);
+?>
---
+++ bitrix/local/templates/mytemplate/description.php
@@ -0,0 +1,9 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+$arTemplate = Array(
+	'NAME' => 'mytemplate',
+	'DESCRIPTION' => 'Шаблон сайта развработавнный по курсу битрикс'
+);
+?>
